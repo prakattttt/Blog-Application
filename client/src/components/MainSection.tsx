ea963e7b95fbbd2228e78896dda23264c39ecb42
prakat <prakat.dhakall64@gmail.com>
2026-05-13 14:07:44 +0545

    fix(font-size): change font-size of main heading and description

--- client/src/components/MainSection.tsx
+++ client/src/components/MainSection.tsx
@@ -5,9 +5,9 @@ const MainSection = ({ header, description }: Content) => {
   return (
     <section className="px-6 py-10 max-w-7xl mx-auto">
       <div className="mb-10">
-        <h1 className="text-5xl font-bold tracking-tight">{header}</h1>
+        <h1 className="text-4xl font-bold tracking-tight">{header}</h1>
 
-        <p className="text-lg text-gray-500 mt-3">{description}</p>
+        <p className="text-md text-gray-500 mt-3">{description}</p>
       </div>
 
       <Cards />
