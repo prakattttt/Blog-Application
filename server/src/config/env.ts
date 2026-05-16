f1d2a6766a63cf23597b7f06957fdabd2d30491e
prakat <prakat.dhakall64@gmail.com>
2026-05-16 21:19:18 +0545

    feat: define secret access token variable from env

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -1,4 +1,7 @@
 const PORT: number = Number(process.env["PORT"]) || 5000;
+
 const DB_URL: string = process.env["DB_URL"] || "";
 
-export { PORT, DB_URL };
+const secretAToken: string = process.env["ACCESS_TOKEN_SECRET"] as string;
+
+export { PORT, DB_URL, secretAToken };
