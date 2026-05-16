633cc4753403c06cfe0ab12436f863403b7734b3
prakat <prakat.dhakall64@gmail.com>
2026-05-16 21:21:41 +0545

    feat: add controller for user registration

--- server/src/controllers/user.controllers.ts
+++ server/src/controllers/user.controllers.ts
@@ -1,10 +1,17 @@
 import expressAsyncHandler from "express-async-handler";
 import type { RequestHandler } from "express";
 import { User } from "../models/user.models.js";
+import { generateAccessTokens } from "../utils/tokenGenerator.js";
+
+export interface registerInterface {
+  name: string,
+  email: string,
+  password: string
+}
 
 export const getAllUsers: RequestHandler = expressAsyncHandler(
   async (req, res) => {
-    const skip = Number(req.query["skip"]) || 0;
+    const skip: number = Number(req.query["skip"]) || 0;
 
     const users = await User.getUsers(skip);
 
@@ -14,3 +21,17 @@ export const getAllUsers: RequestHandler = expressAsyncHandler(
     });
   },
 );
+
+
+export const registerUser: RequestHandler = expressAsyncHandler(
+  async(req, res) => {
+    const { name, email, password }: registerInterface = (req.body || {}) as registerInterface;
+
+    const registerUser: { success: boolean, id: string } = await User.registerUser(name, email, password);
+
+    if(registerUser.success) {
+      const token: string = generateAccessTokens({ id: registerUser.id })
+      res.status(200).json({ token, success: true })
+    }
+  }
+)
