ceeabc5c7ba493b47f0c4b60519f2969d66b1dd6
prakat <prakat.dhakall64@gmail.com>
2026-05-20 00:15:01 +0545

    feat: implementation of useAuth hook

--- client/src/components/Profile.tsx
+++ client/src/components/Profile.tsx
@@ -1,12 +1,20 @@
 import profile from "../assets/profile.jpg";
+import useAuth from "../hooks/useAuth";
+import { FiUser } from "react-icons/fi";
 
 const Profile = () => {
-  return (
+  const { isLoggedIn } = useAuth();
+
+  return isLoggedIn ? (
     <img
       src={profile}
       alt="profile"
       className="w-10 h-10 md:w-10 rounded-full object-cover hover:cursor-pointer hover:scale-105 transition-all duration-300 lg:w-12 lg:h-12"
     />
+  ) : (
+    <div className="w-10 h-10 md:w-10 lg:w-12 lg:h-12 rounded-full flex items-center justify-center bg-gray-200 hover:bg-gray-300 cursor-pointer transition-all duration-300">
+      <FiUser className="text-lg md:text-xl text-black" />
+    </div>
   );
 };
 
