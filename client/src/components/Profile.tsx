fe4a56f31c38bc2dfcf5ea30c2a38a51b6271c42
prakat <prakat.dhakall64@gmail.com>
2026-05-20 18:38:23 +0545

    feat: add link to profile which directs to login

--- client/src/components/Profile.tsx
+++ client/src/components/Profile.tsx
@@ -1,6 +1,7 @@
 import profile from "../assets/profile.jpg";
 import useAuth from "../hooks/useAuth";
 import { FiUser } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
 const Profile = () => {
   const { isLoggedIn } = useAuth();
@@ -12,9 +13,9 @@ const Profile = () => {
       className="w-10 h-10 md:w-10 rounded-full object-cover hover:cursor-pointer hover:scale-105 transition-all duration-300 lg:w-12 lg:h-12"
     />
   ) : (
-    <div className="w-10 h-10 md:w-10 lg:w-12 lg:h-12 rounded-full flex items-center justify-center bg-gray-200 hover:bg-gray-300 cursor-pointer transition-all duration-300">
+    <Link to={"/login"} className="w-10 h-10 md:w-10 lg:w-12 lg:h-12 rounded-full flex items-center justify-center bg-gray-200 hover:bg-gray-300 cursor-pointer transition-all duration-300">
       <FiUser className="text-lg md:text-xl text-black" />
-    </div>
+    </Link>
   );
 };
 
