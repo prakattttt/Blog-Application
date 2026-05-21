8d37feb1f76050b0a3b786caea43b0892aedb39c
prakat <prakat.dhakall64@gmail.com>
2026-05-21 21:38:21 +0545

    fix: include correct backend route

--- client/src/api/auth.api.ts
+++ client/src/api/auth.api.ts
@@ -2,19 +2,19 @@ import api from "./axios";
 import type { LoginBody, RegisterBody } from "../types/auth.types"
 
 export const loginUser = async (body: LoginBody) => {
-  const response = await api.post("/users/login", body);
+  const response = await api.post("/login", body);
 
   return response.data;
 };
 
 export const registerUser = async (body: RegisterBody) => {
-  const response = await api.post("/users/register", body);
+  const response = await api.post("/register", body);
 
   return response.data;
 };
 
 export const logoutUser = async () => {
-  const response = await api.post("/users/logout");
+  const response = await api.post("/logout");
 
   return response.data;
 };
