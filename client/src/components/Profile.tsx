57b8feb54752a513ac9ea432defa798be154542c
prakat <prakat.dhakall64@gmail.com>
2026-05-21 22:05:49 +0545

    feat: add basic ui on hover on profile

--- client/src/components/Profile.tsx
+++ client/src/components/Profile.tsx
@@ -6,16 +6,31 @@ import { Link } from "react-router-dom";
 const Profile = () => {
   const { isLoggedIn } = useAuth();
 
-  return isLoggedIn ? (
-    <img
-      src={profile}
-      alt="profile"
-      className="w-10 h-10 md:w-10 rounded-full object-cover hover:cursor-pointer hover:scale-105 transition-all duration-300 lg:w-12 lg:h-12"
-    />
-  ) : (
-    <Link to={"/login"} className="w-10 h-10 md:w-10 lg:w-12 lg:h-12 rounded-full flex items-center justify-center bg-gray-200 hover:bg-gray-300 cursor-pointer transition-all duration-300">
-      <FiUser className="text-lg md:text-xl text-black" />
-    </Link>
+  return (
+    <>
+      {isLoggedIn ? (
+        <div className="relative group flex items-center justify-center">
+          <img
+            src={profile}
+            alt="profile"
+            className="w-10 h-10 md:w-10 lg:w-12 lg:h-12 rounded-full object-cover cursor-pointer hover:scale-105 transition-all duration-300"
+          />
+
+          <span className="hover-info">Profile</span>
+        </div>
+      ) : (
+        <div className="relative group flex items-center justify-center">
+          <Link
+            to={"/login"}
+            className="w-10 h-10 md:w-10 lg:w-12 lg:h-12 rounded-full flex items-center justify-center bg-gray-200 hover:bg-gray-300 transition-all duration-300"
+          >
+            <FiUser className="text-lg md:text-xl text-black" />
+          </Link>
+
+          <span className="hover-info">Login</span>
+        </div>
+      )}
+    </>
   );
 };
 
