246e07c6f61f375d488365f52156f5331e1d698d
prakat <prakat.dhakall64@gmail.com>
2026-05-21 22:45:30 +0545

    feat(dropdown): add a dropdown that handles logout and navigates to future accoint settings

---
+++ client/src/components/Dropdown.tsx
@@ -0,0 +1,47 @@
+import { FiSettings, FiLogOut } from "react-icons/fi";
+import { Link } from "react-router-dom";
+import { logoutUser } from "../api/auth.api";
+import { useNavigate } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
+
+type DropdownProps = {
+  closeDropdown: () => void;
+};
+
+const Dropdown = ({ closeDropdown }: DropdownProps) => {
+  const { setIsLoggedIn } = useAuth();
+  const navigate = useNavigate();
+
+  function handleLogOut() {
+    closeDropdown();
+    setTimeout(async () => {
+      await logoutUser();
+      setIsLoggedIn(false);
+      navigate("/login");
+    }, 2000);
+  }
+
+  return (
+    <div className="absolute top-15 -left-46 md:right-0 md:translate-x-0 w-60 rounded-2xl bg-white/90 backdrop-blur-xl border border-gray-200 shadow-2xl shadow-black/10 p-2 z-100 animate-[fadeIn_0.2s_ease]">
+      <div className="absolute -top-2 right-6 w-4 h-4 bg-white rotate-45 border-l border-t border-gray-200"></div>
+      <Link
+        to="/settings"
+        onClick={closeDropdown}
+        className="flex items-center gap-3 px-4 py-3 rounded-xl text-sm font-medium text-gray-700 hover:bg-gray-100 transition-all duration-200"
+      >
+        <FiSettings className="text-lg" />
+        Account Settings
+      </Link>
+
+      <button
+        onClick={handleLogOut}
+        className="w-full flex items-center gap-3 px-4 py-3 rounded-xl text-sm font-medium text-red-500 hover:bg-red-50 transition-all duration-200 cursor-pointer"
+      >
+        <FiLogOut className="text-lg" />
+        Logout
+      </button>
+    </div>
+  );
+};
+
+export default Dropdown;
