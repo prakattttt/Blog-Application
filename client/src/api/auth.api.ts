d41f8877d5405f6a71abfa875dd78a38d369d526
prakat <prakat.dhakall64@gmail.com>
2026-05-25 22:33:52 +0545

    fix: change api url in the frontend

--- client/src/api/auth.api.ts
+++ client/src/api/auth.api.ts
@@ -2,19 +2,19 @@ import api from "./axios";
 import type { LoginBody, RegisterBody } from "../types/auth.types";
 
 export const loginUser = async (body: LoginBody) => {
-  const response = await api.post("/login", body);
+  const response = await api.post("/users/login", body);
 
   return response.data;
 };
 
 export const registerUser = async (body: RegisterBody) => {
-  const response = await api.post("/register", body);
+  const response = await api.post("/users/register", body);
 
   return response.data;
 };
 
 export const logoutUser = async () => {
-  const response = await api.post("/logout");
+  const response = await api.post("/users/logout");
 
   return response.data;
 };
@@ -26,7 +26,7 @@ export const getMe = async () => {
 };
 
 export const setBio = async ({ id, bio }: { id: string; bio: string }) => {
-  const response = await api.post(`/set-bio/${id}`, {
+  const response = await api.post(`/users/set-bio/${id}`, {
     bio,
   });
 
