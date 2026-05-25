a53968a66b84bcc5fff0476010a81c21a3270818
prakat <prakat.dhakall64@gmail.com>
2026-05-25 21:56:17 +0545

    feat(post-model): add interfaces, statics for creating and getting the posts

--- server/src/models/post.models.ts
+++ server/src/models/post.models.ts
@@ -1,4 +1,5 @@
-import { model, Schema, Types } from "mongoose";
+import { model, Schema, type Model, Types, isValidObjectId } from "mongoose";
+import AppError from "../utils/AppError.js";
 
 interface IPost {
   title: string;
@@ -9,16 +10,34 @@ interface IPost {
 
   likes: Types.ObjectId[];
 
+  commentsCount: number;
+
   createdAt?: Date;
   updatedAt?: Date;
 }
 
-const PostSchema = new Schema<IPost>(
+interface PostBody {
+  title: string;
+  description: string;
+  imageSrc?: string;
+  author: string;
+}
+
+interface IPostModel extends Model<IPost> {
+  createPost(post: PostBody): Promise<{ postId: string; creatorId: string }>;
+
+  getPosts(skip: number): Promise<IPost[]>;
+
+  getPostsByAuthor(id: string): Promise<IPost[]>;
+}
+
+const PostSchema = new Schema<IPost, IPostModel>(
   {
     title: {
       type: String,
       required: [true, "Title is required!"],
       trim: true,
+      maxlength: 50,
     },
 
     description: {
@@ -43,10 +62,51 @@ const PostSchema = new Schema<IPost>(
         ref: "User",
       },
     ],
+
+    commentsCount: {
+      type: Number,
+      default: 0,
+    },
   },
   {
     timestamps: true,
+
+    statics: {
+      async createPost(post: PostBody) {
+        if (!isValidObjectId(post.author)) {
+          throw new AppError("Invalid author ID!", 400);
+        }
+
+        const createdPost = await this.create({
+          ...post,
+          author: new Types.ObjectId(post.author),
+        });
+
+        return {
+          postId: createdPost._id.toString(),
+          creatorId: createdPost.author.toString(),
+        };
+      },
+
+      async getPosts(skip: number) {
+        return this.find()
+          .populate("author", "username email")
+          .limit(12)
+          .skip(skip * 12)
+          .sort({ createdAt: -1 });
+      },
+
+      async getPostsByAuthor(id: string) {
+        if (!isValidObjectId(id)) {
+          throw new AppError("Invalid ID!", 400);
+        }
+
+        return this.find({ author: id })
+          .populate("author", "username email")
+          .sort({ createdAt: -1 });
+      },
+    },
   },
 );
 
-export const Post = model<IPost>("Post", PostSchema);
+export const Post = model<IPost, IPostModel>("Post", PostSchema);
