0ee3fcd0f9a43f4f5de64960b52cd65aa81fe3c5
prakat <prakat.dhakall64@gmail.com>
2026-05-28 19:05:10 +0545

    feat: set up env configuration for cloudinary

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -6,4 +6,10 @@ const secretAToken: string = process.env["ACCESS_TOKEN_SECRET"] as string;
 
 const env: string = process.env["NODE_ENV"] as string;
 
-export { PORT, DB_URL, secretAToken, env };
+const cloudinary_config: { cloud_name: string, api_key: string, api_secret: string } = {
+    cloud_name: process.env["CLOUDINARY_CLOUD_NAME"],
+    api_key: process.env["CLOUDINARY_API_KEY"],
+    api_secret: process.env["CLOUDINARY_API_SECRET"] 
+} as { cloud_name: string, api_key: string, api_secret: string };
+
+export { PORT, DB_URL, secretAToken, env, cloudinary_config };
