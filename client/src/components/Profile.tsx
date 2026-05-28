d1e08da5003de6122ec29fdf88231fd0ca6c8ee8
prakat <prakat.dhakall64@gmail.com>
2026-05-28 19:09:29 +0545

    feat: integrate the real profile of the user to avatar in the website

--- client/src/components/Profile.tsx
+++ client/src/components/Profile.tsx
@@ -6,9 +6,17 @@ import Dropdown from "./Dropdown";
 import { useState } from "react";
 
 const Profile = () => {
-  const { isLoggedIn, user } = useAuth();
+  const { isLoggedIn, user, loading } = useAuth();
 
   const [openDropdown, setOpenDropdown] = useState(false);
+
+  if (loading) {
+    return (
+      <div className="w-10 h-10 md:w-12 md:h-12 rounded-full bg-gray-200 animate-pulse" />
+    );
+  }
+
+  const profileImage = user?.profileImage ? user.profileImage : profile;
 
   return (
     <>
@@ -16,7 +24,7 @@ const Profile = () => {
         <div className="relative flex items-center justify-center">
           <div className="relative group flex items-center justify-center">
             <img
-              src={user?.profileImage || profile}
+              src={profileImage}
               alt="profile"
               onClick={() => setOpenDropdown((prev) => !prev)}
               className="w-10 h-10 md:w-10 lg:w-12 lg:h-12 rounded-full object-cover cursor-pointer hover:scale-105 transition-all duration-300"
@@ -38,7 +46,7 @@ const Profile = () => {
         <div className="relative group flex items-center justify-center">
           <Link
             to={"/login"}
-            className="w-10 h-10 md:w-10 lg:w-12 lg:h-12 rounded-full flex items-center justify-center bg-gray-200 hover:bg-gray-300 transition-all duration-300"
+            className="w-10 h-10 md:w-12 md:h-12 rounded-full flex items-center justify-center bg-gray-200 hover:bg-gray-300 transition-all duration-300"
           >
             <FiUser className="text-lg md:text-xl text-black" />
           </Link>
