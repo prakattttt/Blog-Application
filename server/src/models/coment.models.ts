61a41671df49eff2b4bbd31fd228cc99223c040e
prakat <prakat.dhakall64@gmail.com>
2026-06-06 23:55:20 +0545

    feat: add the model statics to get the user comments and add the user comments on a post to the database

--- server/src/models/coment.models.ts
+++ server/src/models/coment.models.ts
@@ -1,4 +1,5 @@
-import { model, Schema, Types } from "mongoose";
+import { isValidObjectId, Model, model, Schema, Types } from "mongoose";
+import AppError from "../utils/AppError.js";
 
 interface IComment {
   text: string;
@@ -9,6 +10,21 @@ interface IComment {
 
   createdAt?: Date;
   updatedAt?: Date;
+}
+
+interface PopulatedComment extends IComment {
+  name: string;
+  profileImage: string;
+}
+
+interface ICommentModel extends Model<IComment> {
+  getPostComments(id: string): Promise<PopulatedComment[]>;
+
+  addPostComment(
+    user: string,
+    post: string,
+    comment: string,
+  ): Promise<PopulatedComment[]>;
 }
 
 const CommentSchema = new Schema<IComment>(
@@ -33,6 +49,45 @@ const CommentSchema = new Schema<IComment>(
   },
   {
     timestamps: true,
+  
+    statics: {
+      async getPostComments(id: string) {
+        if (!isValidObjectId(id)) {
+          throw new AppError("Invalid ID!", 400);
+        }
+
+        const postID = new Types.ObjectId(id);
+
+        const comments = await this.find({ post: postID }).populate(
+          "user",
+          "name profileImage",
+);
+
+return comments;
+      },
+
+      async addPostComment(user: string, post: string, comment: string) {
+        if (!isValidObjectId(user) || !isValidObjectId(post)) {
+          throw new AppError("Invalid ID!", 400);
+        }
+
+        const userID = new Types.ObjectId(user);
+        const postID = new Types.ObjectId(post);
+
+        await this.create({
+          text: comment.trim(),
+          user: userID,
+          post: postID,
+        });
+
+        const comments = await this.find({ post: postID }).populate(
+          "user",
+          "name profileImage",
+        );
+
+        return comments;
+      },
+    },
   },
 );
 
