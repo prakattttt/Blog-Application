82c9b1e9586624ab0986f9047dfded2be14470eb
prakat <prakat.dhakall64@gmail.com>
2026-06-19 20:50:10 +0545

    fix: change the input element to text area and other ui elements of comments

--- client/src/components/Comments.tsx
+++ client/src/components/Comments.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { addPostComments, getPostComments } from "../api/comment.api";
 import { FaRegCommentDots } from "react-icons/fa";
 import Loader from "./Loader";
@@ -19,7 +19,14 @@ const Comments = ({
   const [loading, setLoading] = useState(true);
   const [editedText, setEditedText] = useState("");
   const [editCommentId, setEditCommentId] = useState("");
-  // const [deleteComment, setDeleteComment] = useState(false);
+
+  const commentRef = useRef<HTMLTextAreaElement | null>(null);
+  const editRef = useRef<HTMLTextAreaElement | null>(null);
+
+  const autoResize = (textarea: HTMLTextAreaElement) => {
+    textarea.style.height = "auto";
+    textarea.style.height = `${textarea.scrollHeight}px`;
+  };
 
   useEffect(() => {
     const run = async () => {
@@ -44,14 +51,18 @@ const Comments = ({
       setNewComment("");
       return;
     }
+
     try {
       const createdComment = await addPostComments(postID, newComment);
 
       onCommentAdded();
-
       setComments(createdComment);
-
       setNewComment("");
+
+      // reset textarea height after posting
+      if (commentRef.current) {
+        commentRef.current.style.height = "52px";
+      }
     } catch (error) {
       console.log(error);
     }
@@ -60,6 +71,13 @@ const Comments = ({
   const handleEditComment = (id: string, text: string) => {
     setEditCommentId(id);
     setEditedText(text);
+
+    // wait until textarea renders
+    setTimeout(() => {
+      if (editRef.current) {
+        autoResize(editRef.current);
+      }
+    }, 0);
   };
 
   const handleEdit = () => {
@@ -90,14 +108,18 @@ const Comments = ({
             Be the first to share your thoughts.
           </p>
         </div>
+
         <div className="container">
           <div className="mt-6 flex items-center gap-3">
-            <input
-              type="text"
+            <textarea
+              ref={commentRef}
               value={newComment}
-              onChange={(e) => setNewComment(e.target.value)}
+              onChange={(e) => {
+                setNewComment(e.target.value);
+                autoResize(e.target);
+              }}
               placeholder="Write a comment..."
-              className="border rounded-xl px-4 py-3 outline-none transition-all duration-300 border-gray-300 focus:border-black focus:ring-2 focus:ring-black/20"
+              className="border rounded-xl p-4 outline-none transition-all duration-300 border-gray-300 focus:border-black focus:ring-2 focus:ring-black/20 resize-none overflow-hidden min-h-13 w-full"
             />
 
             <button
@@ -124,30 +146,36 @@ const Comments = ({
                   alt="comment-profile"
                   className="w-11 h-11 rounded-full object-cover"
                 />
+
                 <div className="container">
                   <div className="bg-gray-100 rounded-2xl px-4 py-3 flex-1">
                     <h3 className="font-bold text-sm text-black">
                       {item.user.name}
                     </h3>
 
-                    <p className="text-sm text-gray-600 mt-1 leading-relaxed">
+                    <div className="text-sm text-gray-600 mt-1 leading-relaxed">
                       {editCommentId === item._id ? (
                         <>
-                          <input
-                          className="w-full flex-1 border border-gray-300 rounded-2xl px-3 py-2 outline-none focus:ring-2 focus:ring-black/20 focus:border-black transition"
+                          <textarea
+                            ref={editRef}
+                            className="border border-gray-300 rounded-2xl p-4 text-sm leading-tight outline-none focus:ring-2 focus:ring-black/20 focus:border-black transition resize-none overflow-hidden w-full min-h-13"
                             value={editedText}
-                            onChange={(e) => setEditedText(e.target.value)}
+                            onChange={(e) => {
+                              setEditedText(e.target.value);
+                              autoResize(e.target);
+                            }}
                           />
+
                           <div className="flex gap-3 mt-2">
                             <button
-                              className="bg-black text-white rounded-xl px-3 py-1 font-semibold transition-all duration-300 hover:scale-[1.02] active:scale-[0.98] cursor-pointer text-xs"
+                              className="bg-black text-white rounded-xl px-4 py-2 font-semibold transition-all duration-300 hover:scale-[1.02] active:scale-[0.98] cursor-pointer text-xs"
                               onClick={handleEdit}
                             >
                               Save
                             </button>
+
                             <button
-                              className="border border-gray-300 rounded-xl px-3 py-1 font-semibold transition-all duration-300 hover:scale-[1.02] hover:bg-gray-200 active:scale-[0.98] cursor-pointer text-xs;
-"
+                              className="border border-gray-300 rounded-xl px-3 py-1 font-semibold transition-all duration-300 hover:scale-[1.02] hover:bg-gray-200 active:scale-[0.98] cursor-pointer text-xs"
                               onClick={() => setEditCommentId("")}
                             >
                               Cancel
@@ -155,38 +183,44 @@ const Comments = ({
                           </div>
                         </>
                       ) : (
-                        <p>{item.text}</p>
+                        <span>{item.text}</span>
                       )}
-                    </p>
-                  </div>
-                  {item.user._id === user?._id && (
-                    <div className="flex gap-3 ml-4 mt-1">
-                      <span
-                        className="mini-click text-blue-500 hover:text-blue-600"
-                        onClick={() => handleEditComment(item._id, item.text)}
-                      >
-                        Edit
-                      </span>
-                      <span
-                        className="mini-click text-red-500 hover:text-red-600"
-                        onClick={handleDeleteComment}
-                      >
-                        Delete
-                      </span>
                     </div>
-                  )}
+                  </div>
+
+                  {item.user._id === user?._id &&
+                    editCommentId !== item._id && (
+                      <div className="flex gap-3 ml-4 mt-1">
+                        <span
+                          className="mini-click text-blue-500 hover:text-blue-600"
+                          onClick={() => handleEditComment(item._id, item.text)}
+                        >
+                          Edit
+                        </span>
+
+                        <span
+                          className="mini-click text-red-500 hover:text-red-600"
+                          onClick={handleDeleteComment}
+                        >
+                          Delete
+                        </span>
+                      </div>
+                    )}
                 </div>
               </div>
             ))}
           </div>
 
           <div className="mt-6 flex items-center gap-3">
-            <input
-              type="text"
+            <textarea
+              ref={commentRef}
               value={newComment}
-              onChange={(e) => setNewComment(e.target.value)}
+              onChange={(e) => {
+                setNewComment(e.target.value);
+                autoResize(e.target);
+              }}
               placeholder="Write a comment..."
-              className="flex-1 border border-gray-300 rounded-2xl px-4 py-3 outline-none focus:ring-2 focus:ring-black/20 focus:border-black transition"
+              className="flex-1 border border-gray-300 rounded-2xl p-4 text-sm leading-tight outline-none focus:ring-2 focus:ring-black/20 focus:border-black transition resize-none overflow-hidden min-h-13"
             />
 
             <button
